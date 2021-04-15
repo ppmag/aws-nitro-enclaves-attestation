6d8ae14df1b65743b177c14700d612c4596ed62f
ppmag <mybestexpert@gmail.com>
2021-04-15 21:26:08 +0300

    improved C tests

--- ffi/build.rs
+++ ffi/build.rs
@@ -7,6 +7,7 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
+
     let dest_dir = Path::new(&out_dir).join("../../../headers/");
     let dest_path = Path::new(&dest_dir).join("nitroattest.h");
 
@@ -14,7 +15,21 @@ fn main() {
         .expect("Unable to generate bindings")
         .write_to_file(dest_path);
 
-    let include_dir = dest_dir.as_path().to_string_lossy(); //env::var("CARGO_MANIFEST_DIR").unwrap();
+    //let include_dir = dest_dir.as_path().to_string_lossy();
+
+    let include_dir  = String::from(dest_dir.to_str().unwrap());
+    let include_dir2  = String::from(env::var("CARGO_MANIFEST_DIR").unwrap().as_str()) + "/src/";
+    
+    //include_dir += ";";
+    //include_dir += env::var("CARGO_MANIFEST_DIR").unwrap().as_str();
+    //include_dir += "/src";
+    
+    
+    //include_dir.push_str(";");
+    //include_dir.push_str(env::var("CARGO_MANIFEST_DIR").unwrap() + "/src");
+    //include_dir.push(";");
+    //include_dir.join()
+    //let include_dir = include_dir.to_string_lossy();
 
     let mut shared_object_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     shared_object_dir.push(".."); // we need workspace dir
@@ -28,8 +43,10 @@ fn main() {
     // * `-L`, add `shared_object_dir` to library search path,
     // * `-D_DEBUG`, enable debug mode to enable `assert.h`.
     println!(
-        "cargo:rustc-env=INLINE_C_RS_CFLAGS=-I{I} -L{L} -D_DEBUG",
+        "cargo:rustc-env=INLINE_C_RS_CFLAGS=-I{I} -I{I2} -L{L} -D_DEBUG",
         I = include_dir,
+        I2 = include_dir2,
+    
         L = shared_object_dir.clone(),
     );
 
